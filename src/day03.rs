b31e599d2ad1e80c0a9cf8fbbf634b8396326479
Kasil <kasil@loutre.info>
2019-12-12 16:36:16 +0100

    toujours pas fini

--- src/day03.rs
+++ src/day03.rs
@@ -1,43 +1,75 @@
 use std::vec::*;
 use advent_of_code_2019::*;
+use std::cmp::{max,min};
  
+#[derive(std::cmp::PartialEq)]
 enum Orientation{
     Vertical,
     Horizontal,
-};
+}
 
 struct Segment{
     orientation:Orientation,
     origin_x:i32,
     origin_y:i32,
-    lenght:i32,
+    length:i32,
 }
-/*
-impl Segment{
-    pub fn collide(&self,&other:Segment){
-        if self.orientation == other.orientation{
 
-            match self.orientation{
-                Orientation::Horizontal=>{},
-                Orientation::Vertical=>{},
+impl Segment{
+    pub fn collide(&self,other:&Segment)->Option<(i32,i32)>{
+        if self.orientation == other.orientation{
+            if match self.orientation{
+                Orientation::Horizontal=>self.origin_y == other.origin_y,
+                Orientation::Vertical=>self.origin_x == other.origin_x
+            }{
+                
             }
+        }
+        None
+    }
+
+    pub fn get_topleft(&self)->(i32,i32){
+        (self.origin_x,self.origin_y)
+    }
+    pub fn get_bottomright(&self)->(i32,i32){
+        match self.orientation{
+            Orientation::Horizontal=>(self.origin_x + self.length,self.origin_y),
+            Orientation::Vertical=>(self.origin_x,self.origin_y+self.length),
         }
     }
 }
-*/
+
+
+fn get_maxima(path : &Vec<Segment>)->((i32,i32),(i32,i32)){
+    let mut x_min = 0;
+    let mut x_max = 0;
+    let mut y_min = 0;
+    let mut y_max = 0;
+
+    for s in path{
+        let (x,y) = s.get_topleft();
+        x_min = min(x_min,x);
+        y_min = min(y_min,y);
+        let (x,y) = s.get_bottomright();
+        x_max = max(x_max,x);
+        y_max = max(y_max,y);
+    }
+
+    ((x_min,x_max),(y_min,y_max))
+}
 pub struct Day03Input{
     wire1:Vec<Segment>,
     wire2:Vec<Segment>
 }
 
 
-fn parse_line(line:&Vec<&str>)->Vec<Segment>{
+fn parse_line(line:&str)->Vec<Segment>{
     let mut node_x = 0;
     let mut node_y = 0;
     line.split(',')
         .map(str::trim)
-        .filter_map(|s|(s[0], s[1..].parse::<i32>().ok()))
-        .map(|(d,l)|
+        .map(|s|(s.chars().next().unwrap() , s[1..].parse::<i32>().expect("this is wrong")))
+        .map(|(d,l)|{
             let ret = 
                 match d{
                     'U' => Segment{    orientation:Orientation::Vertical, 
@@ -51,99 +83,92 @@ fn parse_line(line:&Vec<&str>)->Vec<Segment>{
                     'L' => Segment{    orientation:Orientation::Horizontal,
                                     origin_y:node_y,
                                     origin_x:node_x - l,
-                                    lenght : l},
+                                    length : l},
                     'R' => Segment{    orientation:Orientation::Horizontal,
                                     origin_y:node_y,
                                     origin_x:node_x,
-                                    length :l},  
+                                    length :l},
+                    _ => panic!("nonsense"),
                 };
             match d{
                 'U' => node_y = node_y - l,
                 'D' => node_y = node_y + l,
                 'L' => node_x = node_x - l,
                 'R' => node_x = node_x + l,
+                _ => panic!("should already have panic ed"),
             };
-            ret)
-        .collect();
+            ret}
+            )
+        .collect()
 }
+
+
+
 
 impl ParsedInput for Day03Input{
     fn parse(input:& String)->Day03Input{
         let lines: Vec<&str> = input.split('\n').map(str::trim).collect();
+        let wire1 = parse_line(lines[0]);
+        let wire2 = parse_line(lines[1]);
+        
 
         Day03Input{
-            wire1: parse_line(lines[0]),
-            wire2: parse_line(lines[1]),
+            wire1,
+            wire2,
         }
     }
 }
-impl std::fmt::Display for Day01Input{
+impl std::fmt::Display for Day03Input{
     fn fmt(&self,f: &mut std::fmt::Formatter)->std::fmt::Result{
-        for v in self.data.iter(){
-            write!(f,"{},",v)?;
-        }
+        let ((x_min,x_max),(y_min,y_max)) = get_maxima(&self.wire1);
+        write!(f,"{} items between X {},{} Y {},{}\n",self.wire1.len(),x_min,x_max,y_min,y_max)?;
+        let ((x_min,x_max),(y_min,y_max)) = get_maxima(&self.wire2);
+        write!(f,"{} items between X {},{} Y {},{}\n",self.wire2.len(),x_min,x_max,y_min,y_max)?;
         Ok(())
     }
 }
-
-pub struct Day01Puzzle{
+/*
+pub struct Day03Puzzle{
     value:i32,
 }
 
-fn secret_calculation(v:&i32)->i32{
-    (v/3) - 2
-}
-
-
-impl PuzzleSolution<Day01Input> for Day01Puzzle{
+impl PuzzleSolution<Day03Input> for Day03Puzzle{
     
-    fn solve(input:&Day01Input)->Day01Puzzle{
-        Day01Puzzle{
+    fn solve(input:&Day03Input)->Day03Puzzle{
+        Day03Puzzle{
             value : input.data.iter()
                               .map(secret_calculation)
                               .sum()
         }
     }
 }
-impl std::fmt::Display for Day01Puzzle{
+
+impl std::fmt::Display for Day03Puzzle{
     fn fmt(&self,f: &mut std::fmt::Formatter)->std::fmt::Result{
         write!(f,"Day 01 Part One : {}",self.value)
     }
 }
 
-fn secret_calculation_plus_fuel(v:&i32)->i32{
-    let mut sum:i32 = 0;
-    let mut current = *v;
-    loop{
-        let new = secret_calculation(&current);
-        if new <= 0 {
-            return sum
-        }else{
-            sum += new;
-            current = new;
-        }
-    }
-}
 
-pub struct Day01PuzzleTwo{
+pub struct Day03PuzzleTwo{
     value:i32,
 }
  
 
-impl PuzzleSolution<Day01Input> for Day01PuzzleTwo{
+impl PuzzleSolution<Day03Input> for Day03PuzzleTwo{
     
-    fn solve(input:&Day01Input)->Day01PuzzleTwo{
-        Day01PuzzleTwo{
+    fn solve(input:&Day03Input)->Day03PuzzleTwo{
+        Day03PuzzleTwo{
             value : input.data.iter()
                               .map(secret_calculation_plus_fuel)
                               .sum()
         }
     }
 }
-impl std::fmt::Display for Day01PuzzleTwo{
+impl std::fmt::Display for Day03PuzzleTwo{
     fn fmt(&self,f: &mut std::fmt::Formatter)->std::fmt::Result{
         write!(f,"Day 01 Part Two : {}",self.value)
     }
 }
-
+*/
 
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use crate::advent_of_code_2019::*;
 use std::fs::read_to_string;
 mod day01;
 mod day02;
+mod day03;
 
 fn main() -> Result<(), std::io::Error> {
 
@@ -12,19 +13,24 @@ fn main() -> Result<(), std::io::Error> {
     let input_day01 = read_to_string("day01.input")?;
     let parsed_input_day01 = day01::Day01Input::parse(&input_day01);
     let solution_day01 = day01::Day01Puzzle::solve(&parsed_input_day01);
-    println!("{}",&solution_day01);
+    println!("{}",&solution_day01);//3275518
     let solution_day01_two = day01::Day01PuzzleTwo::solve(&parsed_input_day01);
-    println!("{}",&solution_day01_two);
+    println!("{}",&solution_day01_two);//4910404
 }
 {
     let input_day02 = read_to_string("day02.input")?;
     let mut parsed_input_day02 = day02::Day02Input::parse(&input_day02);
     parsed_input_day02.parametrize(12, 02);
     let solution_day02 = day02::Day02Puzzle::solve(&parsed_input_day02);
-    println!("{}",&solution_day02);
+    println!("{}",&solution_day02);//2692315
         
     let solution_day02_two = day02::Day02PuzzleTwo::solve(&parsed_input_day02);
-    println!("{}",&solution_day02_two);
+    println!("{}",&solution_day02_two);//9507
+
+    let input_day03 = read_to_string("day03.input")?;
+    let parsed_input_day03 = day03::Day03Input::parse(&input_day03);
+    println!("{}",&parsed_input_day03);
+
 }
     Ok(())
 }
